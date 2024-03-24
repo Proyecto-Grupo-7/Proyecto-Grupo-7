40947cdf75d482cf42d5c17736bfb3db7ed6de2b
danie <danie@LAPTOP-H46NU03M>
2024-03-24 15:16:47 -0500

    Updated

---
+++ src/main/java/org/tp24/ApplicationRunner.java
@@ -0,0 +1,15 @@
+package org.tp24;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ApplicationRunner {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    void run() {
+        LOGGER.info("la aplicación ha sido ejecutada exitosamente");
+        
+    }
+
+}
